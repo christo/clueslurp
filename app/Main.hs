93d211fd32beb95ff5b5d143e222698a5baac33c
Chris Mountford <chris.mountford@gmail.com>
2024-11-21 21:11:04 +1100

    compress

--- app/Main.hs
+++ app/Main.hs
@@ -2,32 +2,37 @@
 
 module Main where
 
-import System.IO (withFile, IOMode(AppendMode), Handle, isEOF, hPutStrLn, stdout, stderr, hSetBuffering, BufferMode(..))
-import Text.Regex.TDFA ((=~))
 import Control.Monad (unless, when)
 import System.Environment (getArgs)
-
+import System.IO (
+  BufferMode (..),
+  Handle,
+  IOMode (AppendMode),
+  hPutStr,
+  hSetBuffering,
+  isEOF,
+  stderr,
+  stdout,
+  withFile,
+ )
+import Text.Regex.TDFA ((=~))
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [clueFile] -> streamIt clueFile
+    [clueFile] -> do
+      hSetBuffering stdout NoBuffering
+      hSetBuffering stderr NoBuffering
+
+      -- clues consume a whole line and look like this:
+      -- DSFSSF.FOO=292@234328|097a2f97afaf97af7af9af7af9af7a9
+      withFile clueFile AppendMode $ \handle -> do
+        processInput "^[A-Z]+\\.[A-Z]+=\\d@\\d+|[0-9a-f]{31}$" handle ""
+    
     _ -> do
       putStrLn "Usage clueslurp <cluefile>"
       putStrLn "  <cluefile>: file containing clues to be read and updated"
-
-streamIt :: FilePath -> IO ()
-streamIt clueFile = do
-  -- clues consume a whole line and look like this:
-  -- DSFSSF.FOO=292@234328|097a2f97afaf97af7af9af7af9af7a9
-  let clueRegex = "^[A-Z]+\\.[A-Z]+=\\d@\\d+|[0-9a-f]{31}$"
-
-  hSetBuffering stdout NoBuffering
-  hSetBuffering stderr NoBuffering
-
-  withFile clueFile AppendMode $ \handle -> do
-    processInput clueRegex handle ""
 
 processInput :: String -> Handle -> String -> IO ()
 processInput regex handle buffer = do
@@ -38,6 +43,6 @@ processInput regex handle buffer = do
     let newBuffer = buffer ++ [char]
     if char == '\n'
       then do
-        when (newBuffer =~ regex) $ hPutStrLn handle newBuffer
+        when (newBuffer =~ regex) $ hPutStr handle newBuffer
         processInput regex handle "" -- reset
       else processInput regex handle newBuffer -- continue
